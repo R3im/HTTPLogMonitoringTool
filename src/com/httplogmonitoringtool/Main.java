1f07e1694c27188868610abe7058837825eef9dd
R3im <r3im.cha@gmail.com>
2018-11-21 07:19:56 +0100

    main class

---
+++ src/com/httplogmonitoringtool/Main.java
@@ -0,0 +1,17 @@
+package com.httplogmonitoringtool;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+	}
+
+}
