355420b6ce96fa6159310e2f8bdf9ca183fb91cd
r3im <r3im.cha@gmail.com>
2018-11-25 14:16:56 +0100

    clean

--- build/docker/monitor_traffic.sh
+++ build/docker/monitor_traffic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 java -jar /usr/src/HTTPTrafficFakeLog.jar &
-echo ....Starts the monitoring (be patient)
+echo ....Starts the monitoring \(be patient\)
 java -jar /usr/src/HTTPLogMonitoringTool.jar 
